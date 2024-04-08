eba215706ffc4ddcd54acaa1266fb574b9c56087
Toorq91 <toorq_eik91@hotmail.com>
2024-04-08 13:42:06 +0200

    x

---
+++ ADHD Norge/info.js
@@ -0,0 +1,92 @@
+const info = {
+    credit: [
+        {
+            name: 'Lenard Adler, MD',
+            title: 'Associate Professor of Psychiatry and Neurology',
+            education: 'New York University Medical School',
+        },
+        {
+            name: 'Ronald C. Kessler, PhD',
+            title: 'Professor, Department of Health Care Policy',
+            education: 'Harvard Medical School',
+        },
+        {
+            name: 'Thomas Spencer, MD',
+            title: 'Associate Professor of Psychiatry',
+            education: 'Harvard Medical School',
+        },
+    ],
+    references: [
+        '1. Schweitzer JB, et al. Med Clin North Am. 2001;85(3):10-I I, 757-777.',
+        '2. Barkley RA. Attention Deficit Hyperactivity Disorder: A Handbook for Diagnosis and Treatment. 2nd ed. 1998.',
+        '3. Biederman J, et al. Am J Psychiatry, 1993:150:1792-1798.',
+        '4. American Psychiatric Association: Diagnostic and Statistical Manual of Mental Disorders, Fourth Edition, Text Revision. Washington,DC, American Psychiatric Association. 2000: 85-93.',
+    ],
+    text: [
+        {
+            header: [ 
+                'Symptomsjekkliste for ADHD- selvrapporteringsskala for voksne (ASRS-v1.1) Instruksjoner'
+            ],
+            desc: [
+                'Spørsmålene på baksiden er utarbeidet for å stimulere til dialog mellom deg og pasientene og å bidra tilå avgjøre om de kan lide av symptomer forbundet med attention-deficit/hyperactivity disorder (ADHD).',
+                'Beskrivelse: Symptomsjekklisten er et instrument som består av de atten DSM-IV-TR-kriteriene. Seksav de atten spørsmålene har vist seg å være mest forutsigbare for symptomer som er overensstemmendemed ADHD. Disse seks spørsmålene utgjør grunnlaget for ASRS v1.1 Screener og er også del A avsymptomsjekklisten. Del B av symptomsjekklisten inneholder de tolv resterende spørsmålene.'
+            ],
+        },
+        {
+            header: [
+                'Instruksjoner:',
+                'Symptomer',
+            ],
+            desc: [
+                '1. Be pasienten fylle ut både del A og del B av symptomsjekklisten ved å krysse av i den ruten sombest beskriver hvor hyppig hvert symptom forekommer.',
+                '2. Legg sammen svarene i del A. Hvis det er fire eller flere kryss i de skraverte rutene i del A, harpasienten symptomer som i stor grad er overensstemmende med ADHD hos voksne. Videreutredning anbefales.',
+                '3. Svarene i del B gir ytterligere informasjon og kan tjene som utgangspunkt for videre utredningav pasientens symptomer. Vær særlig oppmerksom på kryss i skraverte ruter. Frekvensen avangivelsene blir ulikt vektet for de ulike spørsmålene. For de tolv spørsmålene regnes det verkenut samlet poengsum eller anvendes diagnostisk sannsynlighet. Det har vist seg at de seksspørsmålene i del A er de som er mest forutsigbare for ADHD og er best egnet somscreeninginstrument.',
+            ],
+        },
+        {
+            header: [
+                'Funksjonssvekkelse:',
+            ],
+            desc: [
+                '1. Gå gjennom hele symptomsjekklisten med pasienten og vurder graden av funksjonssvekkelsensom er forbundet med symptomene.',
+                '2. Ta arbeid/skole, sosialt liv og familieliv med i betraktningen.',
+                '3. Symptomhyppighet og – styrke henger ofte sammen. Symptomsjekklisten kan derfor være tilhjelp i evaluering av funksjonssvekkelsen. Dersom pasientene har hyppige symptomer, kan detvære nyttig å be dem beskrive hvordan disse problemene har påvirket evnen til å arbeide, gjørehusarbeid eller komme overens med andre mennesker, f.eks. ektefelle/partner, samboer ellerandre nærpersoner.',
+            ],
+        },
+        {
+            header: [
+                'Historie:',
+            ],
+            desc: [
+                '1. Vurder om disse eller lignende symptomer var til stede i barndommen. Voksne med ADHDbehøver ikke formelt ha fått diagnosen som barn. I evalueringen av pasientens historie bør du seetter belegg for tidlig forekommende og langvarige vansker med oppmerksomhet eller problemermed selvkontroll. Enkelte viktige symptomer bør ha forekommet i barndommen, men allesymptomene trenger ikke å ha vært til stede.',
+            ],
+        },
+        {
+            header: [
+                'Nytten av ADHD- screening hos voksne',
+            ],
+            desc: [
+                'Forskning viser at symptomene på ADHD kan vedvare inn i voksen alder og ha betydelig innvirkningpå forholdet til andre mennesker, yrkeskarriere og til og med sikkerheten til de pasientene som lider avdet. 1-4 Fordi denne lidelsen ofte blir misforstått vil mange som lider av den og som ikke mottarrelevant behandling, muligens aldri klare å få vist sitt fulle potensial. En del av problemet er at den kanvære vanskelig å diagnostisere, særlig hos voksne.',
+                'Symptomsjekklisten for ADHD- selvrapporteringsskala for voksne (ASRS-v1.1) er utviklet isamarbeid med Verdens helseorganisasjon (WHO) og Workgroup on Adult ADHD, bestående avfølgende psykiatere og forskere:',
+                'Som behandler kan du bruke ASRS v1.1 som et verktøy i arbeidet med å utrede ADHD hos voksnepasienter. Informasjonen som innhentes ved hjelp av spørreskjemaet, kan indikere behovet for en merinngående utredningssamtale. Spørsmålene i ASRS v1.1 er overensstemmende med kriteriene i DSMIV og er rettet mot hvordan ADHD-symptomene hos voksne viser seg. Innholdet i spørreskjemaetgjenspeiler også hvilken vekt DSM-IV legger på ulike symptomer, funksjonssvekkelser og anamnesefor en korrekt diagnose.',
+                'Det tar ca. 5 minutter å fylle ut sjekklisten, og informasjonen den gir kan være et supplement idiagnostiseringen.',
+            ],
+        },
+    ],
+} 
+
+for (let x = 0; x < 4; x++) {
+    innerhtml =
+    info.text[x].header[0]
+    for (let i = 0; x < info.text.header[x].length; i++) {
+        info.text[x].desc[i]
+    }
+}
+skjema();
+
+info.text.header[4];
+info.text.desc[0];
+info.text.desc[1];
+for (let i = 0; x < info.text.header[x].length; i++) {
+    info.text.header[x].desc[i]
+}
---
+++ ADHD Norge/model.js
@@ -0,0 +1,104 @@
+const model = {
+    app: {
+        page: null
+    },
+    question: [
+        {
+            del A,
+        },
+        {
+            question1: '1. Hvor ofte har du problemer med å avslutte en oppgave etter at de interessante delene er unnagjort?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+        },
+        {
+            question2: '2. Hvor ofte er det vanskelig for deg å få orden på ting når du skal utføre en oppgave som krever organisering?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+
+        },
+        {
+            question3: '3. Hvor ofte har du problemer med å huske avtaler eller forpliktelser?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+        },
+        {
+            queston4: '4. Når du har en oppgave som krever at du tenker nøye igjennom det du skal gjøre, hvor ofte unngår eller utsetter du å begynne på den?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question5: '5. Hvor ofte sitter du og fikler med noe når du må sitte lenge i ro?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question6: '6. Hvor ofte føler du deg overdrevet aktiv og tvunget til å gjøre noe, som om du var drevet av en indre motor?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            del B,
+        },
+        {
+            question7: '7. Hvor ofte gjør du slurvefeil når du må jobbe med en kjedelig eller vanskelig oppgave?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question8: '8. Hvor ofte har du problemer med å holde oppmerksomheten oppe når du gjør kjedelig eller ensformig arbeid?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question9: '9. Hvor ofte har du vansker med å konsentrere deg om hva folk sier, selv når de snakker direkte til deg?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+        },
+        {
+            question10: '10. Hvor ofte har du vanskeligheter med å finne igjen ting hjemme eller på jobb?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question11: '11. Hvor ofte blir du distrahert av aktiviteter eller lyder rundt deg?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question12: '12. Hvor ofte forlater du plassen din på møter eller i andre situasjoner der det forventes at du blir sittende?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+        },
+        {
+            question13: '13. Hvor ofte føler du deg rastløs eller urolig i kroppen?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question14: '14. Hvor ofte har du vanskelig for å ta det med ro og slappe av når du har tid for deg selv?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question15: '15. Hvor ofte opplever du å snakke for mye i sosiale sammenhenger?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4, 
+        },
+        {
+            question16: '16. Hvor ofte opplever du at du fullfører setninger for andre før de rekker å fullføre dem selv?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+        },
+        {
+            question17: '17. Hvor ofte har du problemer med å vente på tur i situasjoner der dette er nødvendig?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 4,
+        },
+        {
+            question18: '18. Hvor ofte avbryter du andre når de holder på med noe?',
+            answer: [1, 2, 3, 4, 5],
+            gray: 3,
+        },
+    ],
+};
